3ccef20e5dbc5b45792175e9dd6aa42e5d56b386
AnilYadav143 <samyaduvanshi26@gmail.com>
2023-02-19 21:16:01 +0530

    category del

--- resources/views/admin/subCategoryList.blade.php
+++ resources/views/admin/subCategoryList.blade.php
@@ -6,6 +6,7 @@
                     <div class="usr-img-frame me-2 rounded-circle">
                         <img alt="avatar" class="img-fluid rounded-circle" src="{{asset('storage/'.$item->category_img) }}">
                     </div>
+                    {{-- <p class="align-self-center mb-0 admin-name"> Tiger </p> --}}
                 </div>
             </td>
             <td>{{$dash}}{{$subcategory->category_name}}</td>
@@ -14,12 +15,12 @@
             <td> {{$subcategory->category_desc??''}}</td>
             <td>
                 <button type="button" class="btn btn-success">Edit</button>
-                <button type="button" class="btn btn-danger">Delete</button>
-                <!-- <form action="{{route('category.destroy',$subcategory->id)}}" onsubmit="return confirm('Are you sure?');" method="POST">
+                <!-- <button type="button" class="btn btn-danger">Delete</button> -->
+                <form action="{{route('category.destroy',$subcategory->id)}}" onsubmit="return confirm('Are you sure?');" method="POST">
                     @csrf
                     @method('DELETE')
                     <button class="btn btn-warning" type="submit">Delete</button>
-                </form> -->
+                </form>
             </td>
 	    @if(count($subcategory->subcategory))
             @include('admin/subCategoryList',['subcategories' => $subcategory->subcategory])
